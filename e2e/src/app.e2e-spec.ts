3a3504af8c7a29c3bd230c4ed5e0b3fb18954656
Marthinus Engelbrecht <marthinus.engelbrecht@protonmail.com>
2019-04-11 05:16:35 +0200

    Initial commit
    Basic structure with travis CI config

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,16 +1,11 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('Stock Search App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
-  });
-
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to stocksearch!');
   });
 
   afterEach(async () => {
